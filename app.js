9a18ef99646d1e0f65436562e4dd27acc6cf6ece
Bunma93 <bunma93@gmail.com>
2024-07-05 16:26:37 +0700

    Js-num3

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 // write code here
+function bonus(){
+    let cost = prompt("ยอดซื้อ-ขาย")
+    let total = Math.floor(cost/100);
+    return total;
+}
+
+let totalbonus = bonus();
+console.log(totalbonus);
+
